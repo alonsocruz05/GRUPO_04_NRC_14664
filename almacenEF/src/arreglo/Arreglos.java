4d4d7b0069a41ea4dfa7cd34a6fe96bffc0e4800
Derick <garicaderickleonardo@gmail.com>
2026-04-12 11:57:40 -0500

    Estructura final corregida: archivos en la raiz

---
+++ almacenEF/src/arreglo/Arreglos.java
@@ -0,0 +1,34 @@
+package arreglo;
+
+import java.util.ArrayList;
+
+import clase.Celular;
+
+public class Arreglos 
+{
+	private ArrayList<Celular>celu;
+	public Arreglos() 
+	{
+		celu = new ArrayList<Celular>();
+		Adicionar(new Celular(101540, "Iphone_15", 2, 5440));
+		Adicionar(new Celular(8012, "Sansumg_S25", 1, 1440));
+		Adicionar(new Celular(19010, "Honor_X7B", 3, 740));
+		Adicionar(new Celular(10810, "Motorola_G54", 10, 840));
+	}
+	public void Adicionar(Celular x)
+	{
+		celu.add(x);
+	}
+	public int Tamaño() {
+		return celu.size();
+	}
+	public Celular Obtener(int x) {
+		return celu.get(x);
+	}
+	public Celular Buscar(int cod) {
+		for (int i = 0; i < Tamaño(); i++) {
+			if(Obtener(i).getCodigo()==cod) return Obtener(i);	
+		}
+		return null;
+	}
+}
