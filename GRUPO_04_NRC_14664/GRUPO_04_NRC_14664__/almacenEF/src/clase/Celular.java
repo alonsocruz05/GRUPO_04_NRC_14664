f4400db7159ca3561b54e724242bff758d8891f1
Derick <garicaderickleonardo@gmail.com>
2026-04-12 11:30:47 -0500

    Limpiando datos

---
+++ GRUPO_04_NRC_14664/GRUPO_04_NRC_14664__/almacenEF/src/clase/Celular.java
@@ -0,0 +1,43 @@
+package clase;
+public class Celular {
+    private int codigo;
+    private String modelo;
+    private int stock;
+    private double precio;
+
+   
+    public Celular (int codigo, String modelo, int stock, double precio) {
+        this.codigo = codigo;
+        this.modelo = modelo;
+        this.stock = stock;
+        this.precio = precio;
+    }
+
+    // Getters y Setters
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(int codigo) {
+        this.precio = codigo;
+    }
+}
